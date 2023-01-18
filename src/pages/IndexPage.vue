48fd6f6c3d3e7ef63f1dccc32837e524f743bad0
nicholaszuccarelli <nicholaszuccarelli@gmail.com>
2023-01-18 14:41:20 +1100

    Initial commit with bug

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,57 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-input
+      v-model="inputVal"
+      outlined
+      label="Focus on me"
+      style="width: 300px;"
+      @focus="onFocus"
+      @blur="onBlur"
+    >
+      <q-menu :model-value="popupOpen" no-parent-event fit no-focus>
+        <q-card>
+          <q-list padding>
+            <q-item
+              v-for="i in 5"
+              :key="i"
+              clickable
+              dense
+              @click="setVal(i)"
+            >
+              <q-item-section>
+                <q-item-label>
+                  {{ i }}
+                </q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-card>
+      </q-menu>
+    </q-input>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage',
+  data () {
+    return {
+      inputVal: '',
+      popupOpen: false
+    }
+  },
+  methods: {
+    setVal (val) {
+      this.inputVal = val
+    },
+    onFocus () {
+      this.popupOpen = true
+    },
+    onBlur () {
+      this.popupOpen = false
+    }
+  }
+})
+</script>
